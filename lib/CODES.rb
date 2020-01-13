77c6442637e159e0d8c8fff88b533aaca424ba08
Maynard Hartman <mjhartman@brighthouse.com>
2020-01-13 08:37:31 +0000

    Automatically backed up by Learn

---
+++ lib/CODES.rb
@@ -0,0 +1,6 @@
+ERROR_CODES = Hash.new({ "#{:UnknownError}" => "001",
+                "#{:UnequalSides}" => "002",
+                "#{:EqualLateral}" => "003",
+                "#{:Osceles}"       =>"004",
+                "#{:Scalene}"       =>"005"
+})
